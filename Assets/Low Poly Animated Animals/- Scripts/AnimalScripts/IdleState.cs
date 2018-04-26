bc7302c5aa10f2050afbea3b13604a3b7bfbf022
Haley Whitman <hwhitman@uoregon.edu>
2018-04-26 00:10:57 -0400

    Start menu creation

---
+++ Assets/Low Poly Animated Animals/- Scripts/AnimalScripts/IdleState.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System;
+
+namespace LowPolyAnimalPack
+{
+  [Serializable]
+  public class IdleState : AnimalState
+  {
+    public float minStateTime = 500f;
+    public float maxStateTime = 600f;
+    [Tooltip("Chance of it choosing this state, in comparion to other state weights.")]
+    public int stateWeight = 20;
+  }
+}
